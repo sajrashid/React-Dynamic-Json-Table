a8ec032c092a2f7a6ea81664620ae77f21c417dd
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-06-20 00:15:50 +0100

    adding reducer functions

--- hookstest/src/components/supertable/children/thead.js
+++ hookstest/src/components/supertable/children/thead.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React,{useState, useReducer} from "react";
 import _ from 'lodash';
 import helper from '../helpers/helper';
-
-const thead = props => {
-    const options = props.options
-    const hiddenColArr = options.hiddenCols || []
-    const columns = Object.keys(props.json[0])
-    const labelColsArr = props.options.labelCols || []
+import { ACTIONS } from '../actions'
+import { useCustomContext } from "../customContext";
+export default function Thead ({sortDispatch}) {
+    const data= useCustomContext()
+    const options = data.options
+    const hiddenColumns = options.hiddenCols || []
+    const columns = Object.keys(data.json[0])
+    const labelColsArr = data.options.labelCols || []
     const styles = options.theadStyles || ''
     const cssClasses = ` ${styles}`
     const createHeader = () => {
         return columns.map((key) => {
-            const isHidden = _.includes(hiddenColArr, key)
+            const isHidden = hiddenColumns.includes(key)
             const isLabel = _.find(labelColsArr, key)
             return isHidden ? null :
-                isLabel ? <th className={cssClasses} id={key} key={key} onClick={props.headerClick} dangerouslySetInnerHTML={helper.createMarkupLiteral(key, isLabel[key], key)}  ></th>
-                    : <th  className={cssClasses}  id={key} key={key} onClick={props.headerClick} >{key}</th>
+                isLabel ? <th className={cssClasses} id={key} key={key} onClick={(e) => sortDispatch({type:ACTIONS.SORT, payload:{id:e.currentTarget.id}})} dangerouslySetInnerHTML={helper.createMarkupLiteral(key, isLabel[key], key)}></th>
+                    : <th  className={cssClasses}  id={key} key={key} onClick={(e) => sortDispatch({type:ACTIONS.SORT, payload:{id:e.currentTarget.id}})} >{key}</th>
         })
     }
 
@@ -24,4 +26,3 @@ const thead = props => {
     )
 
 }
-export default thead
