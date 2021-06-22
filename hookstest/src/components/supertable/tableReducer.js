214c5cc56e457a72cc21f1a972d48d62a012ca42
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-06-22 07:30:35 +0100

    comments

--- hookstest/src/components/supertable/tableReducer.js
+++ hookstest/src/components/supertable/tableReducer.js
@@ -14,75 +14,81 @@ export const TableReducer = (state, action) => {
         const response = await fetch(this.taskUrl);
         return await response.json();
     }
-   
+
     switch (action.type) {
-  
+
         case ACTIONS.INITIALSTATE:
             state.pageable = action.payload.pageable
-            action.payload.pageable ?  state.json = paginate(state.json || [], state.pageSize, 0):state.totalPages = (Math.ceil(state.json.length / state.pageSize))
+            action.payload.pageable ? state.json = paginate(state.json || [], state.pageSize, 0) : state.totalPages = (Math.ceil(state.json.length / state.pageSize))
             return { ...state }
 
-         case  ACTIONS.SEARCH:
-            var result =[]
-            var searchString= action.payload.search
-            state.searchString =searchString
-            if (searchString.length >0) {
-                for(var i=0; i< state.jsonCopy.length; i++) {
-                    var matched =false
-                    for(var item in  state.jsonCopy[i]) {
+        case ACTIONS.SEARCH:
+            var result = []
+            var searchString = action.payload.search
+            state.searchString = searchString
+            if (searchString.length > 0) {
+                for (var i = 0; i < state.jsonCopy.length; i++) {
+                    // matched test flag ensure we match once
+                    // prevents duplicate results 
+                    var matched = false
+                    for (var item in state.jsonCopy[i]) {
                         var str = state.jsonCopy[i][item].toString().toLowerCase()
-                        if(str.includes(searchString.toLowerCase())) {
-                         if(!matched){
-                            result.push(state.jsonCopy[i])
-                            matched=true
-                          }
+                        if (str.includes(searchString.toLowerCase())) {
+                            if (!matched) {
+                                // if the current item is not in results
+                                // push item
+                                // this prevents duplicate results from being
+                                // pushed into the array
+                                result.push(state.jsonCopy[i])
+                                matched = true
+                            }
                         }
                     }
-                  }
-            }else{
-                result=state.jsonCopy
+                }
+            } else {
+                result = state.jsonCopy
             }
-            state.searchFilter=searchString
-            if(state.pageable){
-                state.pageNo=1
-                state.pagerInput=1
-                state.json=(paginate(result, state.pageSize,0))
+            state.searchFilter = searchString
+            if (state.pageable) {
+                state.pageNo = 1
+                state.pagerInput = 1
+                state.json = (paginate(result, state.pageSize, 0))
                 state.totalPages = Math.ceil(result.length / state.pageSize)
 
-            }else{
-            state.json=result
-            state.totalPages = 1
+            } else {
+                state.json = result
+                state.totalPages = 1
             }
             return { ...state }
         case ACTIONS.GOTOPAGE:
-            const gotoPage =action.payload.gotoPage
-            state.pageNo=gotoPage
-            state.pagerInput=gotoPage
-            state.json=(paginate(state.jsonCopy, state.pageSize, gotoPage-1))
+            const gotoPage = action.payload.gotoPage
+            state.pageNo = gotoPage
+            state.pagerInput = gotoPage
+            state.json = (paginate(state.jsonCopy, state.pageSize, gotoPage - 1))
             return { ...state }
 
         case ACTIONS.FIRST:
-            state.pageNo=1
-            state.pagerInput=1
-            state.json=(paginate(state.jsonCopy, state.pageSize, 0))
+            state.pageNo = 1
+            state.pagerInput = 1
+            state.json = (paginate(state.jsonCopy, state.pageSize, 0))
             return { ...state }
 
         case ACTIONS.LAST:
-            state.json=(paginate(state.jsonCopy, state.pageSize, state.totalPages-1))
-            state.pageNo=state.totalPages
-            state.pagerInput=state.totalPages
+            state.json = (paginate(state.jsonCopy, state.pageSize, state.totalPages - 1))
+            state.pageNo = state.totalPages
+            state.pagerInput = state.totalPages
             return { ...state }
 
         case ACTIONS.NEXT:
-            state.pageNo=state.pageNo >= state.totalPages ? state.pageNo : state.pageNo + 1
-            state.pagerInput=state.pageNo
-            state.json=(paginate(state.jsonCopy, state.pageSize, state.pageNo-1))
+            state.pageNo = state.pageNo >= state.totalPages ? state.pageNo : state.pageNo + 1
+            state.pagerInput = state.pageNo
+            state.json = (paginate(state.jsonCopy, state.pageSize, state.pageNo - 1))
             return { ...state }
-            
+
         case ACTIONS.PREVIOUS:
-            state.pageNo= state.pageNo <2 ? state.pageNo : state.pageNo - 1
-            state.pagerInput=state.pageNo
-            state.json=(paginate(state.jsonCopy, state.pageSize, state.pageNo-1))
+            state.pageNo = state.pageNo < 2 ? state.pageNo : state.pageNo - 1
+            state.pagerInput = state.pageNo
+            state.json = (paginate(state.jsonCopy, state.pageSize, state.pageNo - 1))
             return { ...state }
 
         case ACTIONS.SELECTEDROW:
@@ -92,10 +98,10 @@ export const TableReducer = (state, action) => {
         case ACTIONS.SORT:
             let colName = action.payload.id
             let sortDirection = state.sortDirection
-            if(state.pageable){
-            state.jsonCopy.sort(compareValues(colName, sortDirection));
-            state.json = paginate(state.jsonCopy || [], state.pageSize, state.pageNo-1)
-            }else{
+            if (state.pageable) {
+                state.jsonCopy.sort(compareValues(colName, sortDirection));
+                state.json = paginate(state.jsonCopy || [], state.pageSize, state.pageNo - 1)
+            } else {
                 state.json.sort(compareValues(colName, sortDirection));
             }
             state.sortDirection === 'asc' ? state.sortDirection = 'desc' : state.sortDirection = 'asc'
