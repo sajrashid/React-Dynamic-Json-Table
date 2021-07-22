f16d77bfb7653f9abc7d8a6d694cab3f635f9187
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-07-22 01:25:25 +0100

    add reducer

--- src/npm-component/react-dj-table/table.js
+++ src/npm-component/react-dj-table/table.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from 'react'
 
-import {ACTIONS} from '../react-dj-table/reducers/tablereducer'
 import { TableReducer } from '../react-dj-table/reducers/tablereducer'
 
 const Table = (props) => {
@@ -11,16 +10,9 @@ const Table = (props) => {
 
     const [state, dispatch] = useReducer(TableReducer, initialState)
 
-const buttonClick = () =>{
-    dispatch({ type: ACTIONS.CREATESELECTEDROWCOPY, payload: { row: selectedRow } })
-}
 
-return (
-<div>
-    <button onClick={buttonClick}>Increment</button>
-      {state.testValue} 
-</div>
-)
+
+
 
 }
 export default Table
