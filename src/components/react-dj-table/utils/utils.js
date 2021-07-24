868d433816b0506a24c735d4b29048a54f67968c
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-07-24 05:40:52 +0100

    fixed literal lodash removal bug

--- src/components/react-dj-table/utils/utils.js
+++ src/components/react-dj-table/utils/utils.js
@@ -1,11 +1,19 @@
 
+ const get = (obj, path, defaultValue = undefined) => {
+    const travel = regexp =>
+      String.prototype.split
+        .call(path, regexp)
+        .filter(Boolean)
+        .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+    const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
+    return result === undefined || result === obj ? defaultValue : result;
+  };
+
 
 function templateLiteral(template, context = {}) {
   return template.replace(/\$\{\s*(.+?)\s*\}/g, (match, p1) => {
-      //const value = _.get(context, p1, '')
-
-      const value = context?.p?.[1] ?? ''
-
+     // const value = _.get(context, p1, '')
+    var value = get(context, p1, ''); 
       return value === null ? '' : value
   });
 }
