6cdfcef32ccbae12fb2035f818bdeea6387a2b0d
Khalid <km.developer.all@gmail.com>
2021-07-25 03:18:49 +0100

    cs: fixed multiple smells from SonarCloud

--- src/components/react-dj-table/children/cells.js
+++ src/components/react-dj-table/children/cells.js
@@ -7,11 +7,10 @@ import { createMarkupLiteral } from '../utils/utils'
  * Component for displaying cells in the Table.
  *
  * @component
- * 
+ *
  * const state= {...
   }
  * const row = {...
-    
 }
  * return (
  *   <Cells />
@@ -40,47 +39,69 @@ const Cells = ({ state, dispatch, row, editable }) => {
         }
     }
 
-    const createCells = (row) => {
+    const createCells = () => {
         return columns.map((key) => {
 
             const isHidden = hiddenColArr.find(e => e === key) ? true : false;
 
             const isCheckBox = typeof row[key] === "boolean"
 
-            const isCustom = customColArr.find(function (o) { return o.hasOwnProperty(key) })
+            const isCustom = customColArr.find( (o) => { return o.hasOwnProperty(key) })
 
-            const isDate = dateColArr.find(function (b) { return b.hasOwnProperty(key) })
-           
-            
+            const isDate = dateColArr.find( (b) => { return b.hasOwnProperty(key) })
+
             const locale = isDate ? dateColArr[key] : ''
             if (editable) {
                 const editcssClasses = `editInputText ${styles}`
                 const isReadOnly = readOnly.find(e => e === key) ? true : false;
 
-                if (isHidden) return null
-                if (isReadOnly) return <td className={editcssClasses} key={key}> <input name={key} readOnly disabled type="text" value={row[key].toString()}></input> </td>
-                if (isCheckBox && options.checkBox !== false) return <td type="checkbox" className={cssClasses} key={key}> <input name={key} onChange={onChange} type='checkbox' checked={row[key]}></input></td>
-                if (isCustom) return <td className={editcssClasses} key={key}><input type="text" name={key} onChange={onChange} value={row[key].toString()}></input>  </td>
-               if (isDate) return <td className={editcssClasses} key={key}> <input type="text" name={key} onChange={onChange} value={row[key].toString()}></input> </td>
+                if (isHidden) {
+                    return null
+                }
+
+                if (isReadOnly) {
+                    return <td className={editcssClasses} key={key}> <input name={key} readOnly disabled type="text" value={row[key].toString()}></input> </td>
+                }
+
+                if (isCheckBox && options.checkBox !== false) { 
+                    return <td type="checkbox" className={cssClasses} key={key}> <input name={key} onChange={onChange} type='checkbox' checked={row[key]}></input></td>
+                }
+
+                if (isCustom) { 
+                    return <td className={editcssClasses} key={key}><input type="text" name={key} onChange={onChange} value={row[key].toString()}></input>  </td>
+                }
+
+                if (isDate) {
+                    return <td className={editcssClasses} key={key}> <input type="text" name={key} onChange={onChange} value={row[key].toString()}></input> </td>
+                }
+                
                 return <td className={editcssClasses} key={key}> <input name={key} type="text" onChange={onChange} value={row[key].toString()}></input> </td>
             }
-            if (row[key] == null) return null
+            if (row[key] == null) { 
+                return null
+            }
             // we need to test to determine the types
             // pref in some sort of performant order
             // testing for hidden colums first as we can just step out
             // if we match hidden
-            if (isHidden) return null
+            if (isHidden) { 
+                return null
+            }
 
             // next test a bool type as it's quick and we can eliminate as effeciently as possible
-            if (isCheckBox && options.checkBox !== false) return <td className={cssClasses} key={key}> <input readOnly type='checkbox' checked={row[key]}></input></td>
+            if (isCheckBox && options.checkBox !== false) { 
+                return <td className={cssClasses} key={key}> <input readOnly type='checkbox' checked={row[key]}></input></td>
+            }
 
             // Test if it's a custom type
 
             // the markup is sanitised and returned before being inserted into the dom
 
-            if (isCustom) return <td className={cssClasses} key={key} dangerouslySetInnerHTML={createMarkupLiteral(key, isCustom[key], row[key])}></td>
+            if (isCustom) { 
+                return <td className={cssClasses} key={key} dangerouslySetInnerHTML={createMarkupLiteral(key, isCustom[key], row[key])}></td>
+            }
 
-            // Check if  date 
+            // Check if date
             // then go with the user options
             // get the locale is specified from options.dateOptions
 
