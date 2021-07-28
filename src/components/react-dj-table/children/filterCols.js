266cd349e5bacb31eb974df357333dfd7149bbad
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-07-28 19:38:23 +0100

    testing filters

--- src/components/react-dj-table/children/filterCols.js
+++ src/components/react-dj-table/children/filterCols.js
@@ -5,24 +5,44 @@ const FilterCols = ({ state, dispatch }) => {
     const options = state.options
     const customFilterColArr = options.filterCols || []
     const columns = state.filterColobj
+    const cols = Object.keys(columns)
 
     const searchFilterCh = (e) => {
+        console.log(e)
         dispatch({ type: ACTIONS.SEARCH, payload: { search: { searchString: e.currentTarget.value, columns: [e.currentTarget.name] }, searchFilters: true } })
     }
     const filterColsCss = options.filterColsCss || ''
     const cssClasses = `${filterColsCss}`
-    const cols = Object.keys(columns)
     const createFilters = () => {
         let placeholder = `search..${state.jsonCopy.length} records`
 
         return cols.map((key) => {
-            //const isFilterCol = customFilterColArr.find(e => e === key) ? true : false;
-            return <td key={key}>
-                <input className={cssClasses} name={key} id={"f" + key} onChange={searchFilterCh} placeholder={placeholder} type='text' value={cols[key]}></input>
-            </td>
+            const filterCol = customFilterColArr.find((o) => o.hasOwnProperty(key))
+            if (filterCol) {
+                if (filterCol[key].type === 'text') {
+                    return <td key={key}>
+                        <input className={cssClasses} name={key} id={"f" + key} onChange={searchFilterCh} placeholder={placeholder} type='text' value={cols[key]}></input>
+                    </td>
+                }
+                if (filterCol[key].type === 'number') {
+                    return <td key={key}>
+                        <input className={cssClasses} name={key} id={"f" + key} onChange={searchFilterCh} placeholder={placeholder} type='number' value={cols[key]}></input>
+                    </td>
+                }
+                if (filterCol[key].type === 'checkbox') {
+                    return <td key={key}>
+                        <input className={cssClasses} name={key} id={"f" + key} onChange={searchFilterCh} placeholder={placeholder} type='checkbox' value={cols[key]}></input>
+                    </td>
+                }
+                if (filterCol[key].type === 'range') {
+                    return <td key={key}>
+                        <input min={filterCol[key].min} max={filterCol[key].max} className={cssClasses} name={key} id={"f" + key} onChange={searchFilterCh} placeholder={placeholder} type='range' value={cols[key]}></input>
+                    </td>
+                }
 
+            }
 
-            // return <td key={key}></td>
+            return <td key={key}></td>
         })
     }
 
